9cf4e94c8067c3f5bda1a1cf79f2b4d5ab5869f3
Jarrod Parkes <jarrod@udacity.com>
2016-07-14 08:09:46 -0500

    add sandbox as project not submodule

---
+++ SandboxPlayground-Empty/SandboxPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SandboxPlayground
+//
+//  Created by Fernando Rodríguez Romero on 13/05/16.
+//  Copyright © 2016 udacity.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
